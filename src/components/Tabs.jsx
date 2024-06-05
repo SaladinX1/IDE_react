756cc06c73219b579f2563be6013b107bfc8ac73
SaladinX1 <hopefulhorizon01@gmail.com>
2024-06-05 23:26:25 +0200

    mise en place de la recuperation des state tabs et du composant button

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,17 +1,31 @@
 import CodeTab from "./CodeTab"
-
+import { useSelector } from "react-redux"
+import ButtonTab from "./ButtonTab";
+import { useState } from "react";
 
 export default function Tabs() {
+    const tabs = useSelector(state => state.tabs)
+    const [tabIndex, setTabIndex] = useState(tabs[0].id)
+    console.log(tabs);
+
   return (
     <div className='flex grow'>
         <div className='grow flex flex-col w-[175px] shrink-0 text-slate-200'>
-            <button>HTML</button>
-            <button>CSS</button>
-            <button>JS</button>
+            {tabs.map(tab => (
+                <ButtonTab 
+                key={tab.id}
+                id={tab.id}
+                toggleTab={id => setTabIndex(id)}
+                imgUrl={tab.imgUrl}
+                buttonContent={tab.buttonContent}
+                />
+            ))}
         </div>
         <div className="w-full grow relative">
 
-            <CodeTab/>
+            <CodeTab id={tabIndex}
+                        code={tabs.find(obj => obj.id === tabIndex).code}
+            />
         </div>
 
     </div>
