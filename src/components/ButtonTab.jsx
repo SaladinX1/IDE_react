763ee857968c148521cc6b70cec0cfd028a5f7d7
SaladinX1 <hopefulhorizon01@gmail.com>
2024-06-06 00:57:32 +0200

    Mise en place de lespace de code integré et buttons tabs associés

--- src/components/ButtonTab.jsx
+++ src/components/ButtonTab.jsx
@@ -1,10 +1,15 @@
-
+import { useDispatch } from "react-redux"
+import { hidePreview } from "../features/preview"
 
 export default function ButtonTab({id,toggleTab, buttonContent, imgUrl}) {
 
+    const dispatch = useDispatch()
+
   return (
         <button 
-        onClick={() => toggleTab(id)}
+        onClick={() => {
+            dispatch(hidePreview())
+            toggleTab(id)}}
         className="flex items-center px-5 py-3 hover:bg-slate-600 focus:bg-slate-600 outline-none">
             <img src={imgUrl} className="w-5" alt=""  />
             <span className="ml-3 text-slate-100 text-md">{buttonContent}</span>
