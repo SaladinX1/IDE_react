0259e2155caec7c9171eb3a47b49a6584b85f150
SaladinX1 <hopefulhorizon01@gmail.com>
2024-06-06 00:59:48 +0200

    Integration des composant dans App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import Tabs from './components/Tabs'
+import PreviewButton from './components/PreviewButton'
+
 function App() {
   return (
     <div className="bg-zinc-900 min-h-screen flex flex-col">
@@ -6,7 +9,7 @@ function App() {
           <span className="text-base">the <span>SmartIDE</span></span>
         </h1>
         <div className="w-full">
-            <button>Show preview</button>
+            <PreviewButton/>
         </div>
         <div className="w-full flex">
             <div className="ml-auto mr-2">  
@@ -23,6 +26,7 @@ function App() {
             </div>
         </div>
       </div>
+      <Tabs/>
     </div>
   )
 }
